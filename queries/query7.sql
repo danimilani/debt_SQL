2dda669e5f2fbcf8d34f093048cba0f6ba60f068
Daniella Milani <58880353+danimilani@users.noreply.github.com>
2023-01-02 20:28:35 +0000

    Create query7.sql

---
+++ queries/query7.sql
@@ -0,0 +1,8 @@
+SELECT 
+    country_name, 
+    indicator_name
+FROM international_debt
+WHERE debt = (SELECT 
+                 MAX(debt)
+             FROM international_debt
+             WHERE indicator_code='DT.AMT.DLXF.CD');
